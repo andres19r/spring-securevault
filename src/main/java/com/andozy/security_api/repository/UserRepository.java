84f8fea0d9f40d6514e812ff4e71e743a5172054
Andres Rivero <andres19rivero@gmail.com>
2026-06-11 21:45:51 -0400

    feat: implement JpaUserDetailsService and UserRepository for user authentication

---
+++ src/main/java/com/andozy/security_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.andozy.security_api.repository;
+
+import com.andozy.security_api.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/andozy/security_api/service/JpaUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.andozy.security_api.service;
+
+import com.andozy.security_api.auth.SecurityUser;
+import com.andozy.security_api.entity.User;
+import com.andozy.security_api.repository.UserRepository;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new SecurityUser(user);
+    }
+}
