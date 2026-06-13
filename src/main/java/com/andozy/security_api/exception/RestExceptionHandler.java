485e371f605efd4a86e1c4672c5ba9b880baec42
Andres Rivero <andres19rivero@gmail.com>
2026-06-13 00:10:49 -0400

    feat(exception): implement controller advice to handle email in use & validation exceptions

---
+++ src/main/java/com/andozy/security_api/exception/RestExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.andozy.security_api.exception;
+
+import com.andozy.security_api.dto.ErrorResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        var errorDetails = ErrorResponse.builder()
+                .error("Conflict")
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(errorDetails, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        var errorDetails = ErrorResponse.builder()
+                .error("Bad Request")
+                .message("Validation Errors")
+                .errors(errors)
+                .build();
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
